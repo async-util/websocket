fe6708de60197a76eae7c4e40675d3707d596bbf
talrasha007 <talrasha007@gmail.om>
2023-08-20 10:51:23 +0800

    code & exmpale

---
+++ index.d.mts
@@ -0,0 +1,2 @@
+export function getWsEvents(ws: WebSocket): AsyncIterableIterator<String | Blob>;
+export default getWsEvents;
---
+++ index.mjs
@@ -0,0 +1,36 @@
+export async function *getWsEvents(ws) {
+  let resolve, reject, done = false, error;
+  const buffer = [];
+
+  ws.addEventListener('message', ({ data }) => resolve(data));
+  ws.addEventListener('close', () => resolve());
+  ws.addEventListener('error', (e) => reject(e));
+
+  function pushData(data) {
+    if (data !== undefined) buffer.push(data);
+    else done = true;
+  }
+
+  function setError(err) {
+    error = err;
+  }
+
+  while (true) {
+    const data = await new Promise((res, rej) => {
+      if (buffer.length > 0) return res(buffer.shift());
+      if (buffer.length === 0 && done) return res();
+      if (error) return rej(error);
+
+      resolve = res;
+      reject = rej;
+    })
+
+    resolve = pushData;
+    reject = setError;
+    
+    if (data === undefined) break;
+    else yield data;
+  }
+}
+
+export default getWsEvents;
